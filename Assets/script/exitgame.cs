b14d908466f7a5d07b1464a1d8d5bce6d6314d15
Laurenshearman <lauren2.shearman@live.uwe.ac.uk>
2024-02-27 10:54:00 +0000

    say hello at start of game and escape

--- Assets/script/exitgame.cs
+++ Assets/script/exitgame.cs
@@ -7,12 +7,16 @@ public class exitgame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("esc pressed");
+            Application.Quit();
+        }
     }
 }
