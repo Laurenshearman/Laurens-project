9b757825cb8e8d8b9fda4a74136f1b9d16b83159
Laurenshearman <lauren2.shearman@live.uwe.ac.uk>
2024-04-25 12:53:58 +0100

    video player

--- Assets/script/VideoPlay.cs
+++ Assets/script/VideoPlay.cs
@@ -1,31 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Video;
+using UnityEngine.Video; // Import the Video namespace
 
-public class VideoTrigger : MonoBehaviour
+public class videointeratcion : MonoBehaviour
 {
-    public VideoPlayer videoPlayer;
+    public VideoPlayer videoPlayer; // Reference to the VideoPlayer component
+    public GameObject popupPanel; // Reference to the popup panel GameObject
 
-    private bool hasPlayed = false;
-
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
-        if (!hasPlayed && other.CompareTag("Player"))
-        {
-            PlayVideo();
-            hasPlayed = true;
-        }
+        // Disable the popup panel and hide the video player initially
+        popupPanel.SetActive(false);
+        videoPlayer.gameObject.SetActive(false);
     }
 
-    private void PlayVideo()
+    void OnTriggerEnter(Collider other)
     {
-        // Check if the VideoPlayer component is assigned
-        if (videoPlayer != null)
+        if (other.CompareTag("Player"))
         {
+            // Activate the popup panel GameObject
+            popupPanel.SetActive(true);
+
+            // Show the video player GameObject
+            videoPlayer.gameObject.SetActive(true);
+
             // Play the video
             videoPlayer.Play();
         }
     }
-}
 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Deactivate the popup panel GameObject when the player exits the trigger zone
+            popupPanel.SetActive(false);
+
+            // Pause the video
+            videoPlayer.Pause();
+
+            // Hide the video player GameObject
+            videoPlayer.gameObject.SetActive(false);
+        }
+    }
+}
